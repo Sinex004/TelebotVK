340f441f7915c5272d56e9bf82361fe75579bd2f
Nurassyl <47526437+sinex004@users.noreply.github.com>
2019-03-27 14:18:14 +0600

    test: test something

--- Facade.py
+++ Facade.py
@@ -12,26 +12,27 @@ class _Logger(object):
 
 
 class _Bot(object):
-    server = Flask(__name__)
+
 
     def __init__(self):
         self.bot = telebot.TeleBot(misc.token)
         self.bot.enable_save_next_step_handlers(delay=2)
         self.bot.load_next_step_handlers()
+        self.server = Flask(__name__)
 
-    @server.route('/' + misc.token, methods=['POST'])
-    def getMessage(self):
-        self.bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
+        @self.server.route('/' + misc.token, methods=['POST'])
+        def getMessage(self):
+            self.bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+            return "!", 200
 
-    @server.route("/")
-    def webhook(self):
-        self.bot.remove_webhook()
-        self.bot.set_webhook(url='https://telebotvk.herokuapp.com/' + misc.token)
-        return "!", 200
+        @self.server.route("/")
+        def webhook(self):
+            self.bot.remove_webhook()
+            self.bot.set_webhook(url='https://telebotvk.herokuapp.com/' + misc.token)
+            return "!", 200
 
     def bot_start(self):
-        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+        self.server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
     def getBot(self):
         return self.bot
